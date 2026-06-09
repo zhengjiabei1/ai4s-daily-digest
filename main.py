54ef9d819ff58e3de220c2f33575387284535769
郑佳蓓 <zhengjiabei@zhengjiabeideMacBook-Pro.local>
2026-06-09 15:23:21 +0800

    quota: AI4S 6 + 通用AI 2

--- main.py
+++ main.py
@@ -131,8 +131,8 @@ def _select_diverse(articles: list, max_count: int) -> list:
     group_general_ai = {"大模型发布", "开源工具", "行业动态", "融资收购", "政策监管"}
 
     groups = {
-        "ai4s": ([a for a in articles if a.category in group_ai4s], 5),
-        "general_ai": ([a for a in articles if a.category in group_general_ai], 3),
+        "ai4s": ([a for a in articles if a.category in group_ai4s], 6),
+        "general_ai": ([a for a in articles if a.category in group_general_ai], 2),
     }
 
     for key in groups:
